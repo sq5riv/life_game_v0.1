0bb9387f5d8fd11b89534bb1a70d07a25ac36d49
Michał Kalenik <mchl.klnk@gmail.com>
2022-10-26 14:35:36 +0200

    add endpoints blueprints

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_smorest import Api
+from endpoints.endpoints import root as rep
+from endpoints.endpoints import user as uep
+from endpoints.endpoints import card as cep
+from endpoints.endpoints import deck as dep
+from endpoints.endpoints import rules as ruep
+from endpoints.endpoints import game as gep
+
+
+
+
+app = Flask(__name__)
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Lifegame"
+app.config["API_VERSION"] = "v0.1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+
+api.register_blueprint(rep)
+api.register_blueprint(uep)
+api.register_blueprint(cep)
+api.register_blueprint(dep)
+api.register_blueprint(ruep)
+api.register_blueprint(gep)
---
+++ endpoints/endpoints.py
@@ -0,0 +1,81 @@
+from flask_smorest import Blueprint, abort
+from flask.views import MethodView
+
+
+root = Blueprint('root', __name__, description='Main page operations')
+user = Blueprint('user', __name__, description='user operations')
+card = Blueprint('card', __name__, description='card operations')
+deck = Blueprint('deck', __name__, description='deck operations')
+rules = Blueprint('rules', __name__, description='rules operations')
+game = Blueprint('game', __name__, description='game operations')
+
+
+@root.route('/')
+class Main_page(MethodView):
+    def get(self):
+        return 'ala ma kota'
+
+@user.route('/login')
+class User_ops(MethodView):
+    def post(self):
+        return 'Loged in XD'
+    def delete(self):
+        return 'Logout'
+
+@user.route('/new_user')
+class New_user(MethodView):
+    def get(self):
+        return 'Get user info'
+    def post(self):
+        return 'Add new user to database'
+    def put(self):
+        return 'Update user in database'
+    def delete(self):
+        return 'delete user from database'
+
+@card.route('/card')
+class Cards(MethodView):
+    def get(self):
+        return 'returns cards list'
+    def post(self):
+        return 'add new card to database'
+
+@card.route('/cards/<string:card_id>')
+class Card(MethodView):
+    def get(self, card_id):
+        return 'card number {}'.format(card_id)
+    def put(self, card_id):
+        return 'update card {}'.format(card_id)
+    def delete(self, card_id):
+        return 'delete card {}'.format(card_id)
+
+@rules.route('/rules')
+class Rules(MethodView):
+    def get(self):
+        return 'list of rules'
+    def post(self):
+        return 'add new ruleset to database'
+
+@rules.route('/rules/<string:rules_id>')
+class Rules(MethodView):
+    def get(self, rules_id):
+        return 'get ruleset {}'.format(rules_id)
+    def put(self, rules_id):
+        return 'update ruleset {} in database'.format(rules_id)
+    def delete(self, rules_id):
+        return 'delete ruleset {}'.format(rules_id)
+
+@game.route('/game')
+class Games(MethodView):
+    def get(self):
+        return 'list of games'
+    def post(self):
+        return 'Save game, returns game id'
+@game.route('/game/<string:game_id>')
+class Game(MethodView):
+    def get(self, game_id):
+        return 'Gets game state from game {}'.format(game_id)
+    def put(self, game_id):
+        return 'Overwrite game {}'.format(game_id)
+    def delete(self, game_id):
+        return 'Delete game {}'.format(game_id)
